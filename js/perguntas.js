91e6840922dc84121d9cb501a93bb008833530c6
marcomarcovski <161604330+marcomarcovski@users.noreply.github.com>
2024-09-30 09:33:15 -0300

    Update perguntas.js

---
+++ js/perguntas.js
@@ -0,0 +1,61 @@
+export const perguntas = [
+    {
+        enunciado: "Você gosta de volei?",
+        alternativas: [
+            {
+                texto: "Sim, eu gosto",
+                afirmacao: [
+                    "Que legal, você gosta mesmo de volei",
+                    "Que pena, parece que você não gosta de volei"
+                    ]
+            },
+            {
+                texto: "Não,eu detesto",
+                afirmacao: [
+                    "Que pena, parece que você não gosta de volei",
+                    "Que legal, você gosta mesmo de volei"
+                    ]
+            }          
+           
+        ]
+    },
+    {
+        enunciado: "você gosta de ler?",
+        alternativas: [
+            {
+                texto:"Sim, eu gosto",
+                afirmacao: [
+                    "que legal, você gosta de ler",
+                    "Não, detesto ler"
+                    ]
+            },
+            {
+                texto: "Não, detesto ler",
+                afirmacao: [
+                    "que legal, você gosta de ler",
+                    ""Não, detesto ler"
+                    ]
+            }
+        ]
+    },
+    {
+        enunciado: "Você gosta de cantar?",
+        alternativas: [
+            {
+                texto:"Sim, sou um ótimo cantor",
+                afirmacao: [
+                    "Que legal,eu concordo",
+                    "Entendi, cada um tem seu talento"
+                    ]
+            },
+            {
+                texto:"Não, eu prefiro outro talento",
+                afirmacao: [
+                    "Que legal,eu concordo",
+                    "Entendi, cada um tem seu talento"
+                    ]
+            }
+           
+        ]
+    },
+];
